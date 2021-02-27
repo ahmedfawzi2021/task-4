57353319e8db48334717094cd49e9af121a6faa7
ahmedfawzi2021 <79059325+ahmedfawzi2021@users.noreply.github.com>
2021-02-28 00:04:29 +0200

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+my_file = open('/Users/apple/Desktop/machien learning/machien learning diploma/code file','r')
+print (my_file.read())
+my_file.close()
+
+
+# In[ ]:
+
+
+
+
